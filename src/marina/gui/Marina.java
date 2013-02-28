78af4fe820ddd6897310be28ae4fb7a799f010b2
parsah <phossein@gmu.edu>
2013-02-28 10:57:17 -0500

    initial commit - added demo files from original Marina and several bare-bones classes.

---
+++ src/marina/gui/Marina.java
@@ -0,0 +1,28 @@
+package marina.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The application initializer class. 
+ * */
+public class Marina extends Application {
+	
+	/**
+	 * Return version of Marina.
+	 * @return Application version.
+	 * */
+	public String getVersion() {
+		return "1.0";
+	}
+	
+	public static void main(String args[]) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+}
---
+++ src/marina/matrix/Matrix.java
@@ -0,0 +1,14 @@
+package marina.matrix;
+
+/**
+ * The core Marina data-structure associated with storing and 
+ * manipulating values is a matrix. Matrix variants exist, such as
+ * Contingency Matrices and Position Weight Matrices.
+ * <p>
+ * Each Matrix is a multi-dimensional array whereby its specific
+ * elements are populated given user-provided inputs.
+ * @author Parsa Hosseini
+ * */
+public class Matrix {
+
+}
