6bf9f7fd9b8fcf2e8f7092fde37e6e35d417b080
parsah <phossein@gmu.edu>
2013-05-09 12:14:43 -0400

    Only TFBSs rendered over-represented post-quantification will be saved. This is to that ubiquitous TFBS mappings do not get saved.

--- app/output/MappedWriter.java
+++ app/output/MappedWriter.java
@@ -6,17 +6,38 @@ import gui.MarinaGUI;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import parameter.ParameterMap;
+
+import bean.RepresentedMatrixBean;
 import bindingsite.BindingSite;
 
 public class MappedWriter extends TabFormattedWriter {
 	
 	/**
+	 * A useful function which mines only those TFBSs which are rendered
+	 * over-represented.
+	 * @return Set a collection of BindingSite objects set as over-represented.
+	 * */
+	private Set<BindingSite> getAbundantSites() {
+		Set<BindingSite> sites = new HashSet<BindingSite>();
+		List<RepresentedMatrixBean> items = MarinaGUI.get().getTable().getItems();
+		for (RepresentedMatrixBean bean: items) {
+			sites.add(bean.getSite());
+		}
+		return sites;
+	}
+
+	/**
 	 * Writes specific information as far as which 
 	 * */
 	@Override
 	public void writeAll() throws IOException {
+		// first, get a list of only the TFBSs which wbere rendered abundant.
+		Set<BindingSite> abundantSites = this.getAbundantSites();
 		ParameterMap paramMap = MarinaGUI.get().parameterMap();
 		Group[] groups = new Group[]{paramMap.getQuery(), 
 				paramMap.getBaseline()}; // get both groups
@@ -24,14 +45,19 @@ public class MappedWriter extends TabFormattedWriter {
 		for (Group group: groups) {
 			String groupName = group.getBasename();
 			for (FASTASequence seq: group.getParser().getSequences()) {
-				System.out.println("\t" + seq.getHeader());
 				StringBuilder allTFBSs = new StringBuilder();
 				for (BindingSite tfbs: seq.getMappings().keySet()) {
-					allTFBSs.append(tfbs+TabFormattedWriter.TAB);
+					// only save a TFBS to the output mapping file if that 
+					// TFBS is over-represented. Otherwise, spurious TFBS
+					// mappings will be saved and these mappings won't be very
+					// meaningful.
+					if (abundantSites.contains(tfbs)) {
+						allTFBSs.append(tfbs+TabFormattedWriter.TAB);
+					}
 				}
 				writer.write(groupName + TabFormattedWriter.TAB + 
 						seq.getHeader() + TabFormattedWriter.TAB +
-						allTFBSs.toString() + TabFormattedWriter.TAB);
+						allTFBSs.toString().trim() + TabFormattedWriter.TAB);
 				writer.flush();
 				writer.newLine();
 			}
